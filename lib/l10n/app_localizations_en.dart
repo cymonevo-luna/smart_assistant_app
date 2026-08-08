0757d97319f827f42268bdbbe04c4f33ae0df7e2
CymonEvolution <110236942+cymonevolution@users.noreply.github.com>
2026-08-08 14:50:52 +0700

    provision: set app name/title/upload dir for smart_assistant_app

--- lib/l10n/app_localizations_en.dart
+++ lib/l10n/app_localizations_en.dart
@@ -9,7 +9,7 @@ class AppLocalizationsEn extends AppLocalizations {
   AppLocalizationsEn([String locale = 'en']) : super(locale);
 
   @override
-  String get appTitle => 'App Template';
+  String get appTitle => 'Smart Assistant App';
 
   @override
   String get appTagline => 'Tagline goes here';
